e274c8013c57e64a4c1f64ddb9c6b286113a76b6
dbowie04 <lorraynecarolline570@gmail.com>
2022-07-06 11:57:12 -0300

    bah ne

---
+++ jogoteca.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+class jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = jogo('The Sims', 'Simulação de vida', ' PC')
+jogo2 = jogo('Minecraft','Sobrevivência', 'PC' )
+jogo3 = jogo('Pou','Bicho Virtual', 'Celular' )
+jogo4 = jogo('Detroit: Become Human','Ação e Aventura', 'PlayStation 4' )
+
+lista = [jogo1, jogo2, jogo3, jogo4]
+
+@app.route('/inicio')
+def ola():
+    return render_template('lista.html', titulo= 'Meus jogos', jogos = lista)
+app.run()
+
